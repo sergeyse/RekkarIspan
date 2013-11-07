803bdccf0d7c65cc086a0ed995753a8bd65107b4
sergeyse <sergiy@simnet.is>
2013-11-07 08:20:55 +0000

    app modif

--- JavaCRUD/src/app.java
+++ JavaCRUD/src/app.java
@@ -7,6 +7,10 @@ import com.crud.gui.MainWindow3;
 public class app {
 
 	public static void main(String[] args) {
+		 if (args.length == 0) {
+		     System.out.println("\nUsage: java reciver'sEmail");
+		     System.exit(0);
+		   }
 		MainWindow3 mainWindow3 = new MainWindow3();
 	     mainWindow3.setVisible(true);
 	     mainWindow3.setSize(700, 700);
